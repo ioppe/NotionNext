0d7724f2db9501d1b908d20cf89a21ff1d205c0f
ioppe <36251201+ioppe@users.noreply.github.com>
2024-03-27 17:38:09 +0800

    Update Live2D.js

--- components/Live2D.js
+++ components/Live2D.js
@@ -9,44 +9,6 @@ import { useEffect } from 'react'
  * @returns
  */
 export default function Live2D() {
-  const { theme, switchTheme } = useGlobal()
-  const showPet = JSON.parse(siteConfig('WIDGET_PET'))
-  const petLink = siteConfig('WIDGET_PET_LINK')
-
-  useEffect(() => {
-    
-    if (showPet && !isMobile()) {
-      Promise.all([
-        loadExternalResource('@/lib/js/jquery.min.js', 'js'),
-        loadExternalResource('@/lib/js/bootstrap.min.js', 'js'),
-        loadExternalResource('@/lib/js/live2dcubismcore.min.js', 'js'),
-        loadExternalResource('@/lib/js/pixi.min.js', 'js'),
-        loadExternalResource('@/lib/js/live2dcubismframework.js', 'js'),
-        loadExternalResource('@/lib/js/live2dcubismpixi.js', 'js'),
-        loadExternalResource('@/lib/js/charData.js', 'js'),
-        loadExternalResource('@/lib/js/l2d.js', 'js'),
-      ]).then((e) => {
-        if (typeof window?.loadlive2d !== 'undefined') {
-          // https://github.com/xiazeyu/live2d-widget-models
-          try {
-          } catch (error) {
-            console.error('读取PET模型', error)
-          }
-        }
-      })
-    }
-  }, [theme])
-
-  function handleClick() {
-    if (JSON.parse(siteConfig('WIDGET_PET_SWITCH_THEME'))) {
-      switchTheme()
-    }
-  }
-
-  if (!showPet) {
-    return <></>
-  }
-
   return (
     <div class="container">
         <div class="form-group">

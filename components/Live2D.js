555a9b5510f91ed9635f2fa73b005827d543e8df
ioppe <273606283@qq.com>
2024-03-28 15:58:12 +0800

    change

--- components/Live2D.js
+++ components/Live2D.js
@@ -6,70 +6,32 @@ import { useEffect } from 'react'
  */
 export default function Live2D() {
   useEffect(() => {
-    // 加载第一个 JavaScript 文件
-    const script1 = document.createElement('script')
-    script1.src = '/js/jquery.min.js'
-    script1.async = true
-    document.body.appendChild(script1)
-    // 加载第二个 JavaScript 文件
-    const script2 = document.createElement('script')
-    script2.src = '/js/bootstrap.min.js'
-    script2.async = true
-    document.body.appendChild(script2)
+    // 动态创建 script 标签并引入外部 JavaScript 文件
+    const loadScript = src => {
+      const script = document.createElement('script')
+      script.src = src
+      script.async = true
+      document.body.appendChild(script)
+    }
 
-    // 加载第三个 JavaScript 文件
-    const script3 = document.createElement('script')
-    script3.src = '/js/live2dcubismcore.min.js'
-    script3.async = true
-    document.body.appendChild(script3)
-
-    // 加载第三个 JavaScript 文件
-    const script4 = document.createElement('script')
-    script4.src = '/js/pixi.min.js'
-    script4.async = true
-    document.body.appendChild(script4)
-
-    // 加载第三个 JavaScript 文件
-    const script5 = document.createElement('script')
-    script5.src = '/js/live2dcubismframework.js'
-    script5.async = true
-    document.body.appendChild(script5)
-
-    // 加载第三个 JavaScript 文件
-    const script6 = document.createElement('script')
-    script6.src = '/js/live2dcubismpixi.js'
-    script6.async = true
-    document.body.appendChild(script6)
-
-    // 加载第三个 JavaScript 文件
-    const script7 = document.createElement('script')
-    script7.src = '/js/charData.js'
-    script7.async = true
-    document.body.appendChild(script7)
-
-    // 加载第三个 JavaScript 文件
-    const script8 = document.createElement('script')
-    script8.src = '/js/l2d.js'
-    script8.async = true
-    document.body.appendChild(script8)
-
-    // 加载第三个 JavaScript 文件
-    const script9 = document.createElement('script')
-    script9.src = '/js/main.js'
-    script9.async = true
-    document.body.appendChild(script9)
+    // 引入所需的 JavaScript 文件
+    loadScript('/js/jquery.min.js')
+    loadScript('/js/bootstrap.min.js')
+    loadScript('/js/live2dcubismcore.min.js')
+    loadScript('/js/pixi.min.js')
+    loadScript('/js/live2dcubismframework.js')
+    loadScript('/js/live2dcubismpixi.js')
+    loadScript('/js/charData.js')
+    loadScript('/js/l2d.js')
+    loadScript('/js/main.js')
 
     // 清理函数，在组件卸载时移除脚本
     return () => {
-      document.body.removeChild(script1)
-      document.body.removeChild(script2)
-      document.body.removeChild(script3)
-      document.body.removeChild(script4)
-      document.body.removeChild(script5)
-      document.body.removeChild(script6)
-      document.body.removeChild(script7)
-      document.body.removeChild(script8)
-      document.body.removeChild(script9)
+      document.querySelectorAll('script').forEach(script => {
+        if (script.src.includes('/js/')) {
+          document.body.removeChild(script)
+        }
+      })
     }
   }, [])
 

920f1cd13fc5c9fc69f179d1b6f2039e6e246360
ioppe <273606283@qq.com>
2024-03-28 15:48:28 +0800

    change commit

--- components/Live2D.js
+++ components/Live2D.js
@@ -8,54 +8,54 @@ export default function Live2D() {
   useEffect(() => {
     // 加载第一个 JavaScript 文件
     const script1 = document.createElement('script')
-    script1.src = '@/lib/js/jquery.min.js'
+    script1.src = '/js/jquery.min.js'
     script1.async = true
     document.body.appendChild(script1)
     // 加载第二个 JavaScript 文件
     const script2 = document.createElement('script')
-    script2.src = '@/lib/js/bootstrap.min.js'
+    script2.src = '/js/bootstrap.min.js'
     script2.async = true
     document.body.appendChild(script2)
 
     // 加载第三个 JavaScript 文件
     const script3 = document.createElement('script')
-    script3.src = '@/lib/js/live2dcubismcore.min.js'
+    script3.src = '/js/live2dcubismcore.min.js'
     script3.async = true
     document.body.appendChild(script3)
 
     // 加载第三个 JavaScript 文件
     const script4 = document.createElement('script')
-    script4.src = '@/lib/js/pixi.min.js'
+    script4.src = '/js/pixi.min.js'
     script4.async = true
     document.body.appendChild(script4)
 
     // 加载第三个 JavaScript 文件
     const script5 = document.createElement('script')
-    script5.src = '@/lib/js/live2dcubismframework.js'
+    script5.src = '/js/live2dcubismframework.js'
     script5.async = true
     document.body.appendChild(script5)
 
     // 加载第三个 JavaScript 文件
     const script6 = document.createElement('script')
-    script6.src = '@/lib/js/live2dcubismpixi.js'
+    script6.src = '/js/live2dcubismpixi.js'
     script6.async = true
     document.body.appendChild(script6)
 
     // 加载第三个 JavaScript 文件
     const script7 = document.createElement('script')
-    script7.src = '@/lib/js/charData.js'
+    script7.src = '/js/charData.js'
     script7.async = true
     document.body.appendChild(script7)
 
     // 加载第三个 JavaScript 文件
     const script8 = document.createElement('script')
-    script8.src = '@/lib/js/l2d.js'
+    script8.src = '/js/l2d.js'
     script8.async = true
     document.body.appendChild(script8)
 
     // 加载第三个 JavaScript 文件
     const script9 = document.createElement('script')
-    script9.src = '@/lib/js/main.js'
+    script9.src = '/js/main.js'
     script9.async = true
     document.body.appendChild(script9)
 

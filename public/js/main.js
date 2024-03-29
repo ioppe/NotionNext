6b5e22efe5adb6cc32e837582cc279506f742ddc
ioppe <273606283@qq.com>
2024-03-29 19:41:53 +0800

    update

--- public/js/main.js
+++ public/js/main.js
@@ -67,7 +67,7 @@ class Viewer {
       } else {
         if (!this.app.view.parentNode) {
           this.app = new PIXI.Application(1280, 720, {
-            backgroundColor: 0xffffff
+            backgroundColor: 0x000000
           })
           const width = window.innerWidth * 0.3
           const height = (width / 16.0) * 9.0
@@ -75,14 +75,6 @@ class Viewer {
           this.app.view.style.height = height + 'px'
           this.app.renderer.resize(width, height)
           this.canvas.html(this.app.view)
-          this.app.ticker.add(deltaTime => {
-            if (!this.model) {
-              return
-            }
-
-            this.model.update(deltaTime)
-            this.model.masks.update(this.app.renderer)
-          })
         }
         this.app.view.style.width = width + 'px'
         this.app.view.style.height = height + 'px'

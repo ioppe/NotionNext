1193aa73f75b6a2cc8336303d1891d3a4994546e
ioppe <273606283@qq.com>
2024-03-29 16:06:55 +0800

    update

--- public/js/main.js
+++ public/js/main.js
@@ -60,7 +60,7 @@ class Viewer {
       this.app.renderer.resize(width, height)
 
       if (this.model) {
-        this.model.position = new PIXI.Point(width * 0.2, height * 0.4)
+        this.model.position = new PIXI.Point(width * 0.3, height * 0.4)
         this.model.scale = new PIXI.Point(
           this.model.position.x * 0.06,
           this.model.position.x * 0.06

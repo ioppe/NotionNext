9fda9102577bbe36a63d1665cbe5c2a2d6720b19
ioppe <273606283@qq.com>
2024-03-30 20:18:05 +0800

    update

--- public/js/main.js
+++ public/js/main.js
@@ -40,7 +40,7 @@ class Viewer {
 
     this.l2d.load(name, this)
 
-    this.app = new PIXI.Application(1920, 1080, { backgroundColor: 0xffffff })
+    this.app = new PIXI.Application(1920, 1080, { backgroundColor: 'transparent' })
     // 设置视口缩放
     this.app.renderer.roundPixels = true;
     const width = window.innerWidth * scale

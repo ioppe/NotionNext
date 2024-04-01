2ab3c55082a3b7b55133e65164caac93bae33d42
ioppe <273606283@qq.com>
2024-04-01 15:18:28 +0800

    update

--- components/Live2D.js
+++ components/Live2D.js
@@ -1,47 +1,14 @@
 /* eslint-disable no-undef */
 import { useEffect } from 'react'
-import { createView} from '@/public/js/main'
+import { createView } from '@/public/js/main'
 /**
  * 网页动画
  * @returns
  */
 export default function Live2D() {
   useEffect(() => {
-    // 动态创建 script 标签并引入外部 JavaScript 文件
-    // const loadScript = src => {
-    //   return new Promise((resolve, reject) => {
-    //     const script = document.createElement('script')
-    //     script.src = src
-    //     script.async = true
-    //     script.onload = resolve
-    //     script.onerror = reject
-    //     document.body.appendChild(script)
-    //   })
-    // }
-
-    // 引入所需的 JavaScript 文件
-    // loadScript('/js/jquery.min.js')
-    //   .then(() => loadScript('/js/bootstrap.min.js'))
-    //   .then(() => loadScript('/js/live2dcubismcore.min.js'))
-    //   .then(() => loadScript('/js/pixi.min.js'))
-    //   .then(() => loadScript('/js/live2dcubismframework.js'))
-    //   .then(() => loadScript('/js/live2dcubismpixi.js'))
-    //   .then(() => loadScript('/js/charData.js'))
-    //   .then(() => loadScript('/js/l2d.js'))
-    //   .then(() => loadScript('/js/main.js'))
-    //   .catch(error => console.error('Failed to load script:', error))
-
-    // loadScript('/js/main.js')
-
-    createView()
-    // 清理函数，在组件卸载时移除脚本
-    // return () => {
-    //   document.querySelectorAll('script').forEach(script => {
-    //     if (script.src.includes('/js/')) {
-    //       document.body.removeChild(script)
-    //     }
-    //   })
-    // }
+    // 创建live2D
+    // createView()
   }, [])
 
   return (
